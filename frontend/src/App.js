afbb068a9befd6cc6be08b3f77c1f878a76d86dc
6joeb <joe6ball@gmail.com>
2021-03-08 22:47:48 +0000

    Created components folder and LandingPage at route /

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-	return <div>hello world</div>;
-}
+import Landing from "./components/layout/LandingPage";
+
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/' component={Landing} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
