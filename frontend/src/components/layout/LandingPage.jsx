f0082ee7793d1a35c69de92de8f5761471a11ad1
6joeb <joe6ball@gmail.com>
2021-03-10 22:19:08 +0000

    Updated landing page inner overlay styling

--- frontend/src/components/layout/LandingPage.jsx
+++ frontend/src/components/layout/LandingPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-	return <div>Landing page</div>;
+	return (
+		<section className='landing'>
+			<div className='landing-inner'>
+				<h1>Find a friend to share a streaming account with</h1>
+				<div>
+					<Link to='/findafriend' className='button button-primary'>
+						Find a friend
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default LandingPage;
