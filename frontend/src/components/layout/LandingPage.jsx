3ae4d4d6c902e07ae89297c865f952aca1995b82
6joeb <joe6ball@gmail.com>
2021-03-13 16:42:08 +0000

    Updated global font-family and landing inner overlay elements spacing

--- frontend/src/components/layout/LandingPage.jsx
+++ frontend/src/components/layout/LandingPage.jsx
@@ -6,19 +6,21 @@ const LandingPage = () => {
 		<section className='landing'>
 			<div className='landing--overlay m--auto'>
 				<div className='text--centered'>
-					<h1 className='text--xxlarge m--0 p--1'>Stream Share</h1>
+					<h1 className='text--xxlarge m--0 p--1'>
+						Stream Share <i class='fas fa-user-friends'></i>
+					</h1>
 				</div>
 				<div className='landing-info p'>
 					<p className='text--xmedium text--centered m--auto ptop'>Find friends</p>
 					<p className='text--xmedium text--centered m--auto ptop'>
 						Share a streaming account
 					</p>
-					<p className='text--xmedium text--centered m--auto ptop'>Save money!</p>
+					<p className='text--xmedium text--centered m--auto p'>Save money!</p>
 				</div>
 				<div className='text--centered m--auto'>
 					<Link
 						to='/findafriend'
-						className='button button--primary p my--2 text--xmedium'
+						className='button button--primary p my--1 text--xmedium'
 					>
 						Find a Friend
 					</Link>
