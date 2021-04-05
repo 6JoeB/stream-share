f4827be34d681959c63a7ab52e37988ed8b6954f
6joeb <joe6ball@gmail.com>
2021-04-05 16:21:45 +0100

    Preparing for deployement

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,14 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use("/api/users", require("./api/users"));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("../frontend/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`App listening at http://localhost:${PORT}`);
